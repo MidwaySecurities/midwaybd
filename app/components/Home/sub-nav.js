4a8e215b6f3fb27b2660659f1a38b6c2b3bc2c9a
Rahat92 <khrahat92@gmail.com>
2025-07-13 14:07:42 +0600

    add bangla button

--- app/components/Home/sub-nav.js
+++ app/components/Home/sub-nav.js
@@ -6,7 +6,7 @@ const SubNav = () => {
     const {isModalOpen, closeModal, openModal} = useModalClose()
     return (
         <div className="flex overflow-x-auto whitespace-nowrap p-4 bg-gray-100">
-            <div className="inline-block px-4 py-2 bg-white rounded-lg shadow-md mr-2 bg-opacity-0 backdrop-blur-lg">
+            <div className="inline-block px-4 py-2 bg-qtp_btn_bg_color text-white rounded-lg shadow-md mr-2 bg-opacity-0 backdrop-blur-lg">
                 Open Bo
             </div>
             <div className="inline-block px-4 py-2 bg-white rounded-lg shadow-md mr-2">
@@ -14,6 +14,9 @@ const SubNav = () => {
             </div>
             <div className="inline-block px-4 py-2 bg-white rounded-lg shadow-md mr-2">
                 Withdrawals 
+            </div>
+            <div className="inline-block px-4 py-2 bg-white rounded-lg shadow-md mr-2">
+                বাংলা 
             </div>
             <div onClick={() => {
                 openModal()

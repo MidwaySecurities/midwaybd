5113899f5a0a045e17f7964d35b459db72391d8c
Rahat92 <khrahat92@gmail.com>
2025-07-13 12:13:25 +0600

    update footer modal

--- app/components/firstApproachModal.js
+++ app/components/firstApproachModal.js
@@ -1,13 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
 
 const FirstApproachModal = () => {
     return (
-        <div className='fixed bottom-0 left-0 right-0 bg-black/50 z-50'>
-            <div className='bg-white w-full flex justify-center items-center relative'>
-                <div className='absolute right-0 top-[-15px] bg-black text-white w-6 h-6 flex justify-center items-center rounded-full p-1'>x</div>
-                <div className='bg-green-500 w-full py-2'>FirstApproachModal</div>
+        // <div className='fixed bottom-0 left-0 right-0 bg-black/50 z-50'>
+        //     <div className='bg-white w-full flex justify-center items-center relative'>
+        //         <div className='absolute right-0 top-[-15px] bg-black text-white w-6 h-6 flex justify-center items-center rounded-full p-1'>x</div>
+        //         <div className='bg-green-500 w-full py-2'>FirstApproachModal</div>
+        //     </div>
+        // </div>
+        <>
+            <div className="csr112">
+                <Image src="" alt="" className="csr115" />
+                <div className="csr114 text-sm">Invest in Stocks, ETFs, Options
+                    All Commission Free
+                </div>
+                <div className="csr116">Download</div>
+                <i className="csr113"></i>
             </div>
-        </div>
+        </>
     )
 }
 

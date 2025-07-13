b63726967db961ad0f5d389a1b1de10af3ba71a7
Rahat92 <khrahat92@gmail.com>
2025-07-13 13:59:51 +0600

    give download button bg

--- app/components/firstApproachModal.js
+++ app/components/firstApproachModal.js
@@ -14,7 +14,7 @@ const FirstApproachModal = () => {
             <div className="csr112 p-8 z-[5000000]">
                 <Image src={Logo} alt="" className="csr115 p-2 py-3" />
                 <div className="csr114 text-[17px] leading-4">Now trade from anywhere.</div>
-                <div className="csr116">
+                <div className="csr116 bg-qtp_btn_bg_color">
                     <Link target='_blank' href={`https://play.google.com/store/apps/details?id=com.midwaybd.quick_trade_pro&hl=en`}>Download</Link>
                 </div>
                 <i className="csr113"></i>

22cb9527b2bb2db5c7548227268a016b2447388e
Rahat92 <khrahat92@gmail.com>
2025-07-14 16:07:29 +0600

    updae hyperlink

--- app/components/Home/Regulators.js
+++ app/components/Home/Regulators.js
@@ -1,5 +1,6 @@
 'use client'
 import Image from "next/image";
+import Link from "next/link";
 import Slider from "react-slick"
 
 
@@ -13,32 +14,38 @@ const Regulators = () => {
     slidesToScroll: 1,
     variableWidth: true
   };
-  
+
   return (
     <div className="text-black mb-12 mt-12">
       <h1 className="text-center text-black pb-4 text-2xl mt-8">Our Regulators</h1>
       <Slider {...settings}>
         <div className="mx-2 inline-block">
           <div className='w-[100px] h-[100px] py-3 rounded-full bg-[#F8F7F5] hover:bg-[#DBEAFE] hover:drop-shadow-lg duration-500 flex justify-center items-center flex-col'>
-            <Image src={`https://cdbl.com.bd/assets/img/CDBL%20Logo.png`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            <Link href={`https://cdbl.com.bd`} target="_blank"><Image src={`https://cdbl.com.bd/assets/img/CDBL%20Logo.png`} alt="cdbl image" width={80} height={80} className="w-[70px]" /></Link>
           </div>
         </div>
 
         <div className="mx-2 inline-block">
           <div className='w-[100px] h-[100px] py-3 rounded-full bg-[#F8F7F5] hover:bg-[#DBEAFE] hover:drop-shadow-lg duration-500 flex justify-center items-center flex-col'>
-            <Image src={`https://www.dsebd.org/assets/images/plc-logo.png`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            <Link href={`https://www.dsebd.org/`} target="_blank">
+              <Image src={`https://www.dsebd.org/assets/images/plc-logo.png`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            </Link>
           </div>
         </div>
 
         <div className="mx-2 inline-block">
           <div className='w-[100px] h-[100px] py-3 rounded-full bg-[#F8F7F5] hover:bg-[#DBEAFE] hover:drop-shadow-lg duration-500 flex justify-center items-center flex-col'>
-            <Image src={`https://sec.gov.bd/images/BSEC-LOGO.png`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            <Link href={`https://www.sec.gov.bd/`} target="_blank">
+              <Image src={`https://www.sec.gov.bd/images/logo.png`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            </Link>
           </div>
         </div>
 
         <div className="mx-2 inline-block">
           <div className='w-[100px] h-[100px] py-3 rounded-full bg-[#F8F7F5] hover:bg-[#DBEAFE] hover:drop-shadow-lg duration-500 flex justify-center items-center flex-col'>
-            <Image src={`https://upload.wikimedia.org/wikipedia/commons/6/63/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6_%E0%A6%AC%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%82%E0%A6%95%E0%A7%87%E0%A6%B0_%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%A4%E0%A7%80%E0%A6%95.svg`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            <Link href={`https://www.bb.org.bd/`} target="_blank">
+              <Image src={`https://upload.wikimedia.org/wikipedia/commons/6/63/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6_%E0%A6%AC%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%82%E0%A6%95%E0%A7%87%E0%A6%B0_%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%A4%E0%A7%80%E0%A6%95.svg`} alt="cdbl image" width={80} height={80} className="w-[70px]" />
+            </Link>
           </div>
         </div>
 

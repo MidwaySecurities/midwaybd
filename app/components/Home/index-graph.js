649db297df9355538b7ebe948bbffb3e037ec8d3
Rahat92 <khrahat92@gmail.com>
2025-07-14 14:46:38 +0600

    add graph

--- app/components/Home/index-graph.js
+++ app/components/Home/index-graph.js
@@ -1,76 +1,61 @@
 'use client'
-import React, { Component } from 'react';
-import CanvasJSReact from '@canvasjs/react-charts';
-//var CanvasJSReact = require('@canvasjs/react-charts');
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+// import faker from 'faker';
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
-class IndexGraph extends Component {	
-	render() {
-		const options = {
-			animationEnabled: true,
-			theme: "light2",
-			title:{
-				text: "Market statistics"
-			},
-			axisX:{
-				valueFormatString: "DD MMM",
-				crosshair: {
-					enabled: true,
-					snapToDataPoint: true
-				}
-			},
-			axisY: {
-				title: "Closing Price (in EUR)",
-				valueFormatString: "€##0.00",
-				crosshair: {
-					enabled: true,
-					snapToDataPoint: true,
-					labelFormatter: function(e) {
-						return "€" + CanvasJS.formatNumber(e.value, "##0.00");
-					}
-				}
-			},
-			data: [{
-				type: "area",
-				xValueFormatString: "DD MMM",
-				yValueFormatString: "€##0.00",
-				dataPoints: [
-				  { x: new Date("2018-03-01"), y: 85.3},
-				  { x: new Date("2018-03-02"), y: 83.97},
-				  { x: new Date("2018-03-05"), y: 83.49},
-				  { x: new Date("2018-03-06"), y: 84.16},
-				  { x: new Date("2018-03-07"), y: 84.86},
-				  { x: new Date("2018-03-08"), y: 84.97},
-				  { x: new Date("2018-03-09"), y: 85.13},
-				  { x: new Date("2018-03-12"), y: 85.71},
-				  { x: new Date("2018-03-13"), y: 84.63},
-				  { x: new Date("2018-03-14"), y: 84.17},
-				  { x: new Date("2018-03-15"), y: 85.12},
-				  { x: new Date("2018-03-16"), y: 85.86},
-				  { x: new Date("2018-03-19"), y: 85.17},
-				  { x: new Date("2018-03-20"), y: 85.99},
-				  { x: new Date("2018-03-21"), y: 86.1},
-				  { x: new Date("2018-03-22"), y: 85.33},
-				  { x: new Date("2018-03-23"), y: 84.18},
-				  { x: new Date("2018-03-26"), y: 85.21},
-				  { x: new Date("2018-03-27"), y: 85.81},
-				  { x: new Date("2018-03-28"), y: 85.56},
-				  { x: new Date("2018-03-29"), y: 88.15}
-				]
-			}]
-		}
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
 
-		return (
-		<div className='bg-white p-4 rounded-lg shadow-md mt-4'>
-			<CanvasJSChart options = {options} 
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Dataset 2',
+    //   data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+      data: labels.map(() => Math.floor(Math.random() * 100)),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+function IndexGraph() {
+  return <Line options={options} data={data} />;
 }
 
-export default IndexGraph;                              
+export default IndexGraph;

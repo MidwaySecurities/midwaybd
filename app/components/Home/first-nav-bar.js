a299fc7b568eae67ef13f556c2d43e36475dd894
Rahat92 <khrahat92@gmail.com>
2025-07-17 13:49:09 +0600

    update font family in footer modal

--- app/components/Home/first-nav-bar.js
+++ app/components/Home/first-nav-bar.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import Logo from "./logo";
 
 const FirstNavBar = () => {
     return (
         <div className="flex justify-between items-center bg-[#9999a1] p-4 text-white">
-            <Logo />
+            <Link href={`/`}>
+                <Logo />
+            </Link>
             <p className="bg-[#1E293B] p-2 px-3 rounded-md">Login</p>
         </div>
     )

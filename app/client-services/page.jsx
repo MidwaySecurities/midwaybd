c1488cc89409fdf613c75786b708f6f9dc484f44
Rahat92 <khrahat92@gmail.com>
2025-07-29 13:34:12 +0600

    update padding in client service

--- app/client-services/page.jsx
+++ app/client-services/page.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 
 const ClientServices = () => {
     return (
-        <div>
-            <div className='p-2'>
+        <div className='pt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg'>
                 <h1 className='text-2xl font-semibold mb-8'>QuickTrade Pro mobile app</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/published/quicktrade-pro-reel-1-facebook-ad.jpg?1737528553`} className='w-full pb-6' width={500} height={500} />
                 <p>
@@ -21,67 +21,67 @@ const ClientServices = () => {
                 </div>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>​MIDWAY PORTAL</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/homepage-4_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>All our services are available online. Get full control of your BO Account. Trade Online, deposit funds, withdraw funds, IPO, SME, ATB and so on. </p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>BO Account Opening</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/bo-image_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>Open a BO Account with us through one of our convenient locations in many parts of the country. Or you can start the process <Link href={`https://www.midwaybd.com/open-a-bo-account.html`} target='_blank' className='text-[##A88D2E]' >online.</Link></p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>Apply for IPO</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/ipo-11_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>You can now apply for any IPO through us via this website. We even offer an IPO membership club.</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>Share Trading</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/buy-and-sell_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>Buy and Sell shares through us over the phone, the internet or visiting any of our offices.</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>IPO Membership Club</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/editor/ipo-club-membership-logo-2.png?1617384776`} className='w-full pb-6' width={500} height={500} />
                 <p>​Join our IPO membership club and you will never have to worry about missing an IPO application. We will automatically apply for all the IPOs for you (depending on your available balance). Please be aware, we will not automatically apply for Mutual Funds. Call or visit us to find out more. </p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>Easy Deposits</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/bank-mobile-money-credit-card-1_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>​Join our IPO membership club and you will never have to worry about missing an IPO application. We will automatically apply for all the IPOs for you (depending on your available balance). Please be aware, we will not automatically apply for Mutual Funds. Call or visit us to find out more. </p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>Tax Report</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/tax-report_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>​Get your yearly tax report on your investments. Get all your documents online.</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>Daily Emails</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/_________4719675.jpg`} className='w-full pb-6' width={500} height={500} />
                 <p>​Receive free daily emails of your portfolio, ledger and trading confirmation. </p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>Easy Withdrawal: BEFTN</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/published/bangladesh-bank-logo-vector-tranparent.png?1617383868`} className='w-2/3 pb-6 m-auto' width={500} height={500} />
                 <p>​Withdraw your funds from the convenience of your home. Email us your signed Cheque Requisition Form and we will directly deposit your funds in your bank account through the approved Bangladesh Electronic Fund Transfer Network (BEFTN). </p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>​DIVIDEND STATEMENT</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/homepage-5_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>​​Never miss your dividends. We provide a yearly statement of all your dividends</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>​CDBL - SMS Alert</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/editor/cdbl-sms-alerts.png?1504609219`} className='w-full pb-6' width={500} height={500} />
                 <p>​​CDBL offers FREE SMS Alerts to the Beneficiary Owner (BO) Account holders' mobile phones giving details of debit-credit transactions that have taken place in their accounts.</p>
@@ -89,25 +89,25 @@ const ClientServices = () => {
                 <Link className='break-words text-[#C4982E]' href='https://www.cdbl.com.bd/sms_balance.php'>https://www.cdbl.com.bd/sms_balance.php</Link>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>​DAILY EMAILS & SMS ALERT</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/email-sms-alert_orig.jpg`} className='w-full pb-6' width={500} height={500} />
                 <p>​​​​Receive free daily emails of your portfolio, ledger and trading confirmation. You will also receive free SMS alert service anytime you buy or sell stocks.</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4'>​​PROFESSIONAL STAFF</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/team_orig.png`} className='w-full pb-6' width={500} height={500} />
                 <p>​​​​Each of our staff are professionally trained to help you succeed in your investment venture. Get in touch with us and we shall help you plan out an investment future.</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4 uppercase'>​​dse training academy</h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/__557534_orig.jpg`} className='w-full pb-6' width={500} height={500} />
                 <p>​​​​Interested in learning more about the Stock Market? Wish to participate in training courses about Portfolio Management? Then the DSE Training Academy is the place for you. DSE Training Academy has faculties and the speakers drawn from the industry, a rich pool of professionals, practitioners and eminent personalities, who mix the theory with practical aspects through which participants get hands on knowledge & experiences shared by the experts. The organization conducts monthly training programs for anyone who is willing to learn or enhance their market knowledge. Regular courses include programs on <strong>Fundamental Stock Analysis, Technical Stock Trading, Equity Portfolio Management, Valuation of Shares</strong> and many more. There are a number of courses and diplomas available through this wonderful <strong>Training Academy</strong>. You can check out the monthly schedule by <Link href={`https://www.dsebd.org/Training_Academy_2.b.php`} target='_blank' className='text-[#C4982E]'>clicking here</Link>. And if you have selected the ones you like, just <Link className='text-[#C4982E]' href={`https://main.dsebd.org/online_login.php`}>click here</Link> to register. Anyone can apply.</p>
             </div>
 
-            <div className='mt-8 px-2'>
+            <div className='p-4 card-shadow rounded-lg mt-4'>
                 <h1 className='text-2xl font-semibold mb-4 uppercase'>​​DSE Info App </h1>
                 <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/published/dse-info-app-1.png?1559838881`} className='w-2/3 pb-6 m-auto' width={500} height={500} />
                 <p>​​​​Download the <strong>DSE INFO App</strong> for your Android devices from the Google Play Store to stay up to date on the Market. This app is developed jointly by the <strong>Dhaka Stock Exchange</strong> and the <strong>ICT Division</strong> of the <strong>Government of Bangladesh</strong>. You can create your own portfolio, track individual stocks, view latest exchange news or look at the daily market snapshot. Download now by clicking <Link href={`https://play.google.com/store/apps/details?id=com.mcc.dhakastockexchangeinfo`}>here.</Link></p>

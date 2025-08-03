81a2cb41dbc129ee0a26112d0b9293e8f9ccccaf
Rahat92 <khrahat92@gmail.com>
2025-08-03 16:59:45 +0600

    form download ...

--- app/foreign-investors/page.jsx
+++ app/foreign-investors/page.jsx
@@ -4,18 +4,18 @@ import React from 'react'
 const ForeignInvestor = () => {
     return (
         <div className='px-4'>
-            <div className='mt-8 card-shadow p-2 rounded-lg'>
+            <div className='mt-4 card-shadow p-4 rounded-lg'>
                 <h1 className='font-bold text-xl mb-3'>Investment Freedom in Bangladesh</h1>
                 <p>The Foreign Investment Act of 1980 guarantees the right of repatriation of invested capital, profits, capital gains, post-tax dividends, and approved royalties and fees. Foreign firms are able to repatriate funds without much difficulty, provided the appropriate documentation is in order. </p>
                 <p>​For Foreign individual the tax on cash dividend is 25%, and tax on capital gain is 0%. For foreign company, the tax on cash dividend is 20%, and capital gain tax is 10%.</p>
             </div>
 
-            <div className='mt-8 card-shadow p-2 rounded-lg'>
+            <div className='mt-4 card-shadow p-4 rounded-lg'>
                 <h1 className='font-bold text-xl mb-3'>Economic Freedom in Bangladesh</h1>
                 <p>Bangladesh's economic freedom score is 55.0, making its economy the 128th freest in the 2017 Index. Its overall score is 1.7 points higher than last year, mainly reflecting improvements in trade freedom and investment freedom. Bangladesh is ranked 28th out of 43 countries in the Asia–Pacific region. Bangladesh has enjoyed impressive economic growth of around 6.5 percent per year over the past five years, driven mainly by its limited but growing services and industrial sectors.</p>
             </div>
 
-            <div className='mt-8 card-shadow p-2 rounded-lg'>
+            <div className='mt-4 card-shadow p-4 rounded-lg'>
                 <h1 className='font-bold text-xl mb-3'>Required Documents</h1>
                 <ol>
                     <li>Collect foreign institutional form</li>
@@ -29,7 +29,7 @@ const ForeignInvestor = () => {
                 </ol>
             </div>
 
-            <div className='mt-8 card-shadow p-2 rounded-lg'>
+            <div className='mt-4 card-shadow p-4 rounded-lg'>
                 <h1 className='font-bold text-xl mb-3'>What we do for our foreign clients </h1>
                 <ol>
                     <li>Dealings with foreign individual/institutional investors.</li>
@@ -46,12 +46,11 @@ const ForeignInvestor = () => {
                 <h1 className='font-bold text-xl'>Required Link</h1>
                 <Link className='font-bold underline wrap-break-word' href={`http://www.dsebd.org/pdf/facilities%20for%20for%20foreign%20investors.pdf`}>http://www.dsebd.org/pdf/facilities%20for%20for%20foreign%20investors.pdf</Link>
             </div>
-
+            
             <div className='mt-8'>
                 <h1 className='font-bold text-xl'>International Broker</h1>
                 <p>As a leading of broker of Dhaka Stock Exchange , we welcome the international broker as partnership basis for mutual benefit . We hope your effort with us will be benefited for your clients' and investors' interest .</p>
             </div>
-
             <p className='font-semibold mt-16 italic text-gray-500'><blockquote className='bg-[url(https://www.midwaybd.com/files/theme/images/quotes.png?1753622919)] bg-no-repeat pl-[30px] text-'>We have several lucrative product design to satisfy our foreign individual or institutional investors .</blockquote></p>
         </div>
     )
---
+++ app/form-download/page.js
@@ -0,0 +1,69 @@
+import Link from 'next/link'
+import React from 'react'
+
+const FormDownload = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-2xl font-bold mb-6 mt-8 px-8'>BO Account</h1>
+                <div className='grid grid-cols-1 gap-4 px-8'>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>BO Account Opening Form</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Authorization Form</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​Change সঞ্চয় to Active</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​Change Address</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​Change Mobile Number</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​Change Email Address</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​​Change সঞ্চয় to Active</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​​Change Address</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​​Change Mobile Number</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>​​Change Email Address</Link></button>
+                </div>
+            </div>
+            
+            <div>
+                <h1 className='text-2xl font-bold mb-6 mt-8 px-8'>Link BO Account</h1>
+                <div className='grid grid-cols-1 gap-4 px-8'>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>DPA 6 (CDBL)</Link></button>
+                </div>
+            </div>
+
+            <div>
+                <h1 className='text-2xl font-bold mb-6 mt-8 px-8'>Accounts</h1>
+                <div className='grid grid-cols-1 gap-4 px-8'>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>
+                        Change Bank Account</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Cheque Requisition Form</Link></button>
+                </div>
+            </div>
+
+            <div>
+                <h1 className='text-2xl font-bold mb-6 mt-8 px-8'>IPO</h1>
+                <div className='grid grid-cols-1 gap-4 px-8'>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>IPO Membership Club Form</Link></button>
+                </div>
+            </div>
+
+            <div>
+                <h1 className='text-2xl font-bold mb-6 mt-8 px-8'>DSE Mobile</h1>
+                <div className='grid grid-cols-1 gap-4 px-8'>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>DSE Mobile App Registration</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>DSE Mobile User Manual</Link></button>
+                </div>
+            </div>
+
+            <div>
+                <h1 className='text-2xl font-bold mb-6 mt-8 px-8'>Corporate</h1>
+                <div className='grid grid-cols-1 gap-4 px-8'>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Acquisition Cost Declaration</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Declaration of Sponsors/Directors</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Declearation of Placement Holder</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Change Commission</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Share Transfer (Gift)</Link></button>
+                    <button className='p-3 bg-gray-300 text-gray-800 font-semibold uppercase rounded-lg'><Link href={`/`}>Share Transfer (non Gift)</Link></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FormDownload

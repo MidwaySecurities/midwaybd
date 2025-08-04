fc0d59076d6af5f451d7de106acaa7b119733f1e
Rahat92 <khrahat92@gmail.com>
2025-08-05 01:28:22 +0600

    update made in navigation menu

--- app/components/modal/navigation.js
+++ app/components/modal/navigation.js
@@ -15,7 +15,7 @@ const Navigation = () => {
         <div style={{ backdropFilter: 'blur(20px)' }} className={`fixed ${isModalOpen ? '' : 'hidden'} top-0 bottom-0 justify-center left-0 right-0 z-[100]  text-foreground text-2xl overflow-scroll`}>
             <BodyScrollLock lock={isModalOpen} />
             <CloseButton />
-            <div className='w-full px-8 mt-2'>
+            <div className='w-full px-8 mt-2 mb-8'>
                 <div className='flex justify-between items-center flex-col px-2 gap-8 rounded-lg w-full'>
                     <div className='flex space-y-2 flex-col mt-8 w-full'>
                         <Link onClick={() => closeModal()} href="/" className='hover:text-green-500 border-2 border-yellow-500 rounded-lg px-3'>Home</Link>

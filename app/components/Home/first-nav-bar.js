1e0749290b35605371a8631e2d1865a4bf83f1c5
Rahat92 <khrahat92@gmail.com>
2025-08-14 17:40:58 +0600

    update style

--- app/components/Home/first-nav-bar.js
+++ app/components/Home/first-nav-bar.js
@@ -4,8 +4,10 @@ import Logo from "./logo";
 const FirstNavBar = () => {
     return (
         <div className="flex justify-between items-center bg-[#9999a1] p-4 text-white">
-            <Link href={`/`}>
+            <Link className="flex items-center gap-2 font-bold text-gray-800 text-lg" href={`/`}>
                 <Logo />
+                {/* <h1>Midway Securities Ltd.</h1> */}
+                <h1 className="flex flex-col leading-5"><span className="tracking-wider">Midway</span><span>Securities Ltd.</span></h1>
             </Link>
             <p className="bg-[#1E293B] p-2 px-3 rounded-md"><Link href={`https://portal.midwaybd.com/bo/portal-login`}>Login</Link></p>
         </div>

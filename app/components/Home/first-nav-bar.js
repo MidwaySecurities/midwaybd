8c253aabf1455ce43452924a1ead82c78170fe99
Rahat92 <khrahat92@gmail.com>
2025-08-25 15:52:53 +0600

    update logo portion

--- app/components/Home/first-nav-bar.js
+++ app/components/Home/first-nav-bar.js
@@ -6,8 +6,9 @@ const FirstNavBar = () => {
         <div className="flex justify-between items-center bg-[#9999a1] p-4 text-white">
             <Link className={`${style.logoZoom} flex items-center gap-2 font-bold text-gray-800 text-[14px]`} href={`/`}>
                 <Logo />
-                {/* <h1>Midway Securities Ltd.</h1> */}
-                <h1 className="flex flex-col leading-5 justify-center uppercase"><span className="tracking-[12.4px]">Midway</span><span className="tracking-[.9px]">Securities Ltd.</span></h1>
+                {/* <h1 className="">Midway Securities Ltd.</h1> */}
+                {/* <h1 className="flex flex-col leading-5 justify-center uppercase"><span className="tracking-[12.4px]">Midway</span><span className="tracking-[.9px]">Securities Ltd.</span></h1> */}
+                <h1 className="flex flex-col leading-5 justify-center uppercase"><span className="text-xl tracking-[2px]">Midway</span><span className="tracking-[.9px] text-[.6rem]">Securities Ltd.</span></h1>
             </Link>
             <p className="bg-[#1E293B] p-2 px-3 rounded-md"><Link href={`https://portal.midwaybd.com/bo/portal-login`}>Login</Link></p>
         </div>

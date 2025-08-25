516478dcc65032322eaec205471d6279af7923e2
Rahat92 <khrahat92@gmail.com>
2025-08-25 14:32:43 +0600

    overflow x-hide

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -7,10 +7,10 @@ const BlogPage = async ({ params }) => {
     const blog = await getABlog(slug);
     console.log("blog", blog);
     return (
-        <div className='max-w-screen-xl m-auto mt-4 p-4'>
+        <div className='m-auto mt-4 p-4'>
             <h1 className='text-2xl font-bold mb-4'>{blog?.blog?.title}</h1>
             <img src={blog?.blog?.coverImage} alt={blog?.blog?.title} className='w-full h-auto object-cover mb-4' />
-            <div className='text-gray-800' dangerouslySetInnerHTML={{ __html: blog?.blog?.content }}></div>
+            <div className='text-gray-800 w-full overflow-x-hidden' dangerouslySetInnerHTML={{ __html: blog?.blog?.content }}></div>
 
             {/* return to all blogs page */}
             <div className='mt-8'>

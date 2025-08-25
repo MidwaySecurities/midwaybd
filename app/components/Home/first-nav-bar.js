6de36d4b2b6f2bad478a5631582bc3be825da860
Rahat92 <khrahat92@gmail.com>
2025-08-25 16:00:56 +0600

    update made in logo section

--- app/components/Home/first-nav-bar.js
+++ app/components/Home/first-nav-bar.js
@@ -8,7 +8,7 @@ const FirstNavBar = () => {
                 <Logo />
                 {/* <h1 className="">Midway Securities Ltd.</h1> */}
                 {/* <h1 className="flex flex-col leading-5 justify-center uppercase"><span className="tracking-[12.4px]">Midway</span><span className="tracking-[.9px]">Securities Ltd.</span></h1> */}
-                <h1 className="flex flex-col leading-0 justify-center uppercase"><span className="text-xl tracking-[2px]">Midway</span><span className="tracking-[.93px] text-[.6rem] leading-2">Securities Ltd.</span></h1>
+                <h1 className="flex flex-col leading-0 justify-center uppercase mt-[-.25rem]"><span className="text-xl tracking-[2px]">Midway</span><span className="tracking-[.93px] text-[.6rem] leading-2">Securities Ltd.</span></h1>
             </Link>
             <p className="bg-[#1E293B] p-2 px-3 rounded-md"><Link href={`https://portal.midwaybd.com/bo/portal-login`}>Login</Link></p>
         </div>

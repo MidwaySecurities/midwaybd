d945571db95c257cf82c3e41f1ba3c2219fc5019
Rahat92 <khrahat92@gmail.com>
2025-09-02 12:46:57 +0600

    update blog category

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,17 +1,26 @@
 import { getBlogs } from '@/lib/actions/blog/getBlogs'
 import Link from 'next/link';
 import React from 'react'
+import Tabs from '../components/blogTab';
+import BlogCategoryDropDown from '../components/blogCategoryDropDown';
 
-const Blogs = async () => {
+const Blogs = async ({ searchParams }) => {
+    const activeCity = searchParams.tab?.toLowerCase() || "beginner";
+    console.log(activeCity)
 
-    const blogs = await getBlogs();
-    console.log(blogs);
-
+    const blogs = await getBlogs({ tag: activeCity });
     return (
         <div>
+            <div className='m-4'>
+                <Tabs activeCity={activeCity} />
+            </div>
+            <div className='m-4'>
+                <BlogCategoryDropDown />
+            </div>
             {blogs?.blogs?.map((blog) => {
                 return (
                     <div key={blog._id} className='border p-4 m-4 rounded-lg shadow-lg'>
+
                         <h2 className='text-2xl font-bold mb-2'>{blog.title}</h2>
                         <img src={blog.coverImage} alt={blog.title} className='w-full h-auto object-cover mb-4' />
                         <div className='text-gray-800'>{blog.excerpt}</div>
---
+++ app/components/blogTab.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import styles from "./Tabs.module.css";
+
+const tabs = [
+  { id: "beginner", label: "Beginner" },
+  { id: "regular", label: "Regular" },
+  { id: "advance", label: "Advance" },
+];
+
+export default function Tabs({ activeCity }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const changeTab = (tabId) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("tab", tabId);
+    router.push(`/blogs/?${params.toString()}`, { scroll: false });
+  };
+
+  return (
+    <div className={`${styles.tab} rounded-t-lg font-semibold`}>
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          className={activeCity === tab.id ? styles.active : ""}
+          onClick={() => changeTab(tab.id)}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  );  
+}

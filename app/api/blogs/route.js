a31ecdb8c72c72a7ae90bd1bddd859c91fe4a515
Rahat92 <khrahat92@gmail.com>
2025-09-07 13:18:20 +0600

    apply api with filter

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -1,7 +1,10 @@
 import { getBlogs } from "@/lib/actions/blog/getBlogs";
 
-export async function GET() {
-// get blogs from database
-  const blogs = await getBlogs();
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const tab = searchParams.get('tab') || 'latest';
+  const category = searchParams.get('category') || 'Select Category';
+  // get blogs from database
+  const blogs = await getBlogs({ tag: tab, category });
   return Response.json(blogs)
 }
--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,60 +1,12 @@
-// import { getBlogs } from '@/lib/actions/blog/getBlogs'
-// import Link from 'next/link';
-// import React from 'react'
-// import Tabs from '../components/blogTab';
-// import BlogCategoryDropDown from '../components/blogCategoryDropDown';
-
-// const Blogs = async ({ searchParams }) => {
-//     const activeCity = searchParams.tab?.toLowerCase() || "beginner";
-//     const activeCategory = searchParams.category || "Others";
-//     console.log(activeCity, activeCategory)
-
-//     // const blogs = await getBlogs({ tag: activeCity, category: activeCategory });
-//     const blogs = await fetch(`http://localhost:3000/api/blogs`, {
-//         next: {
-//             revalidate: 60
-//         }
-//     }).then(res => res.json());
-//     console.log('haha', blogs)
-//     return (
-//         <div>
-//             <div className='m-4'>
-//                 <div className='flex justify-start items-center border-2 border-[#ccc] rounded-t-lg'>
-//                     <Tabs activeCity={activeCity === 'latest' ? 'regular' : activeCity} />
-//                     <BlogCategoryDropDown activeCategory={activeCategory} />
-//                 </div>
-//             </div>
-//             <div className='m-4'>
-//             </div>
-//             {blogs?.blogs?.length>0?blogs?.blogs?.map((blog) => {
-//                 return (
-//                     <div key={blog._id} className='border p-4 m-4 rounded-lg shadow-lg'>
-
-//                         <h2 className='text-2xl font-bold mb-2'>{blog.title}</h2>
-//                         <img src={blog.coverImage} alt={blog.title} className='w-full h-auto object-cover mb-4' />
-//                         <div className='text-gray-800'>{blog.excerpt}</div>
-//                         <Link href={`/blogs/${blog.slug}`} className='text-blue-500 hover:underline mt-4 inline-block'>Read More</Link>
-//                     </div>
-//                 )
-//             }):(
-//                 <div className='text-center text-gray-500 my-20'>No blogs found.</div>
-//             )}
-//         </div>
-//     )
-// }
-
-// export default Blogs
-
 import Link from 'next/link'
 import Tabs from '../components/blogTab'
 import BlogCategoryDropDown from '../components/blogCategoryDropDown'
 
 const Blogs = async ({ searchParams }) => {
-    const { tab } = await searchParams || 'latest'
+    const { tab, category } = await searchParams || 'latest'
     const activeCity = tab?.toLowerCase() || 'beginner'
-    const { category } = await searchParams || 'Others'
-    const activeCategory = category || 'Others'
-
+    const activeCategory = category || 'Select Category'
+    
     const blogs = await fetch(`http://localhost:3000/api/blogs?tab=${activeCity}&category=${activeCategory}`, {
         next: { revalidate: 60 },
     }).then((res) => res.json())

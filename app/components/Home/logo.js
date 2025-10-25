ba3ad705d182e639cdecc474b80d4e7adbbbf955
Rahat92 <khrahat92@gmail.com>
2025-10-26 00:53:49 +0600

    update logo

--- app/components/Home/logo.js
+++ app/components/Home/logo.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Logo = ({width, height}) => {
   return (
     <div>
-        <Image src={`https://www.midwaybd.com/uploads/6/0/4/6/60462737/midway-logo-moto.png`} width={width || 30} height={height || 30} alt='midway logo' />
+        <Image src={`/images/midway-logo-moto.png`} width={width || 30} height={height || 30} alt='midway logo' />
     </div>
   )
 }

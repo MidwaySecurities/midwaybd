96009caec8245893e03a09572c638e45b3457466
MidwaySecurities <it@midwaybd.com>
2026-05-11 15:36:42 +0600

    update tag manager

--- app/layout.js
+++ app/layout.js
@@ -29,33 +29,28 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      </head>
-      <body className={`${poppins.className} antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Script
-          id="gtm-script"
+          id="google-tag-manager"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];
-      w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});
-      var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-      j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-      f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-XXXXXXX');
-    `,
+          (function(w,d,s,l,i){w[l]=w[l]||[];
+          w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T89TGQ8B');
+        `,
           }}
         />
+      </head>
+      <body className={`${poppins.className} antialiased`}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T89TGQ8B"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
         <ModalCloseProvider>
           <BottomModalCloseProvider>
             <DeviceDetectContextProvider>

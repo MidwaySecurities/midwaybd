e9faa6a009b0af82139ea5d0181e65c8ab5c534c
MidwaySecurities <it@midwaybd.com>
2026-09-08 17:37:56 +0600

    update

--- app/page.js
+++ app/page.js
@@ -284,7 +284,8 @@ export default async function Home() {
         </section>
 
         {/* Account Opening Steps */}
-        <section className="py-16 lg:py-24 bg-gradient-to-br from-gray-50 to-blue-50">
+        {/* <section className="py-16 lg:py-24 bg-linear-to-br from-gray-50 to-blue-50"> */}
+        <section className="py-16 lg:py-24 bg-gray-50">
           <div className="container mx-auto px-4">
             <AccountOpenningSteps />
           </div>

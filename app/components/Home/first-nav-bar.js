b06c53370d6606845f944732312f232a404c2008
MidwaySecurities <it@midwaybd.com>
2026-09-17 17:31:20 +0600

    fix title tracking issue

--- app/components/Home/first-nav-bar.js
+++ app/components/Home/first-nav-bar.js
@@ -15,18 +15,18 @@ const FirstNavBar = () => {
     return (
         <div className="lg:container lg:m-auto flex justify-between items-center bg-white p-4 text-white">
             <Link className={`${style.logoZoom} flex items-center gap-2 font-bold text-gray-800 text-[14px]`} href={`/`}>
-                <Logo width = {55} height = {55} />
+                <Logo width={55} height={55} />
                 {/* <h1 className="">Midway Securities Ltd.</h1> */}
                 {/* <h1 className="flex flex-col leading-0 justify-center uppercase mt-[-.25rem]"><span className="text-xl tracking-[2px]">Midway</span><span className="tracking-[.93px] text-[.6rem] leading-2">Securities Ltd.</span></h1> */}
                 {/* need a gap after midway */}
             </Link>
-<h1 className="flex shrink-0 flex-col lg:flex-row items-center lg:items-baseline leading-0 justify-center uppercase -mt-1 text-primary_color font-bold">
-  <span className="text-3xl lg:text-4xl tracking-[2px]">Midway</span>
-  <span className="hidden lg:inline">{"\u00a0\u00a0\u00a0"}</span>
-  <span className="text-[.6rem] lg:text-3xl xl:text-4xl tracking-[4.8px] lg:tracking-[2px] leading-2 lg:leading-none">
-    Securities Ltd.
-  </span>
-</h1>
+            <h1 className="flex shrink-0 flex-col lg:flex-row items-center lg:items-baseline leading-0 justify-center uppercase -mt-1 text-primary_color font-bold">
+                <span className="text-3xl lg:text-4xl tracking-[2px]">Midway</span>
+                <span className="hidden lg:inline">{"\u00a0\u00a0\u00a0"}</span>
+                <span className="text-[.6rem] lg:text-3xl xl:text-4xl tracking-[4.5px] lg:tracking-[2px] leading-2 lg:leading-none">
+                    Securities Ltd.
+                </span>
+            </h1>
             <p className="bg-secondary_color p-2 px-3 rounded-md"><Link href={`https://portal.midwaybd.com`}>Login</Link></p>
         </div>
     )

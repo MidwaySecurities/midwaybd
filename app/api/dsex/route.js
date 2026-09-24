593f577aa5548fab7ab0c1702a7e9f8821782d75
MidwaySecurities <it@midwaybd.com>
2026-09-24 10:05:11 +0600

    update dse url

--- app/api/dsex/route.js
+++ app/api/dsex/route.js
@@ -9,7 +9,7 @@ const agent = new https.Agent({ rejectUnauthorized: false });
 
 export async function GET() {
   try {
-    const res = await axios.get("https://www.dsebd.org/", {
+    const res = await axios.get("https://old.dsebd.org/", {
       httpsAgent: agent,
       headers: {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
